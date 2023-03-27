b8f3efa2b43fcac5684470ebaf9613b3afd6d846
Deepu Mohan Puthrote <git@deepumohan.com>
2023-03-27 22:53:55 +0100

    Initial commit

---
+++ doc/intro.md
@@ -0,0 +1,3 @@
+# Introduction to warfox/kafkapp-clj
+
+TODO: write [great documentation](http://jacobian.org/writing/what-to-write/)
---
+++ test/warfox/kafkapp_clj_test.clj
@@ -0,0 +1,7 @@
+(ns warfox.kafkapp-clj-test
+  (:require [clojure.test :refer :all]
+            [warfox.kafkapp-clj :refer :all]))
+
+(deftest a-test
+  (testing "FIXME, I fail."
+    (is (= 0 1))))
